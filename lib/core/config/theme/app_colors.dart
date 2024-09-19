2fdc2cf513057371d769019c9ca31a06c5aad9c6
Francis <furandoryun@gmail.com>
2024-09-20 01:49:21 +0800

    app theme colors, includes every assets

---
+++ lib/core/config/theme/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+
+  static const background = Color(0xffFFFFFF);
+  static const button_1 = Color(0xff81B257);
+  static const text_onboard = Color(0xffFFFFFF);
+  static const text_onboard_2 = Color(0xff000000);
+  static const plate_number = Color(0xff00930F);
+  static const placeholder = Color(0xffFFFFFF);
+  static const placeholder_boarder = Color(0xff545454);
+  static const navbar_buttons = Color(0xff000000);
+  static const icons = Color(0xff000000);
+  static const mainpage_text = Color(0xff000000);
+  static const carousel_background = Color(0xffDCE4D0);
+  static const carousel_text = Color(0xff000000);
+  static const switch_button = Color(0xff81B257);
+  static const settings_buttonsbg = Color(0xffFFFFFF);
+  static const settings_text_proper = Color(0xff545454);
+  static const drop_shadows = Color(0xffB6B6B6);
+  static const location_marker = Color(0xff81B257);
+  static const truck_marker = Color(0xff19211A);
+}
