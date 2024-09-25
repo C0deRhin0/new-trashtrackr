bb7ead8c483c5fe054dfef3062715443015b2a03
Francis <furandoryun@gmail.com>
2024-09-25 23:14:42 +0800

    added sign up and sign files, sign up will be polished later on

---
+++ lib/presentation/pages/auth/signup.dart
@@ -0,0 +1,157 @@
+// ignore_for_file: unnecessary_const
+
+import 'package:flutter/material.dart';
+import 'package:new_trashtrackr/core/config/assets/app_vectors.dart';
+import 'package:new_trashtrackr/core/config/theme/app_colors.dart';
+import 'package:new_trashtrackr/core/config/theme/app_theme.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SignupPage extends StatelessWidget {
+  SignupPage({super.key});
+
+  final TextEditingController _name = TextEditingController();
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  final TextEditingController _confirmPassword = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: signInLink(context),
+      backgroundColor: AppColors.background,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(
+              vertical: 100,
+              horizontal: 70,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  AppVectors.logo,
+                  height: 100,
+                  width: 100,
+                ),
+                _registerTitle(),
+                const SizedBox(height: 20),
+                _nameField(context),
+                const SizedBox(height: 20),
+                _emailField(context),
+                const SizedBox(height: 20),
+                _passwordField(context),
+                const SizedBox(height: 20),
+                _confirmPasswordField(context),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Register',
+                      style: TextStyle(
+                        color: AppColors.textInButton
+                      )),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(200, 50),
+                    backgroundColor: AppColors.switchButton,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _registerTitle() {
+    return const Text(
+      'Register',
+      style: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _nameField(BuildContext context) {
+    return TextField(
+      controller: _name,
+      decoration: const InputDecoration(
+        hintText: 'Enter name',
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme,
+      ),
+    );
+  }
+
+  Widget _emailField(BuildContext context) {
+    return TextField(
+      controller: _email,
+      decoration: const InputDecoration(
+        hintText: 'Enter Email',
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme,
+      ),
+    );
+  }
+
+  Widget _passwordField(BuildContext context) {
+    return TextField(
+      controller: _password,
+      decoration: const InputDecoration(
+        hintText: 'Enter Password',
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme,
+      ),
+    );
+  }
+
+  Widget _confirmPasswordField(BuildContext context) {
+    return TextField(
+      controller: _confirmPassword,
+      decoration: const InputDecoration(
+        hintText: 'Confirm Password',
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme,
+      ),
+    );
+  }
+
+
+  Widget signInLink(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Already have an account?',
+            style: TextStyle(
+              fontSize: 16,
+              color: AppColors.mainpageText,
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => SignupPage(),
+                ),
+              );
+            },
+            child: const Text(
+              'Sign In',
+              style: TextStyle(
+                fontSize: 16,
+                color: AppColors.switchButton,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
