b7aa7869c21b0fd1adee063fbbc1172b95c0546a
Francis <furandoryun@gmail.com>
2024-09-25 23:14:06 +0800

    asset access for logo added

---
+++ lib/core/config/assets/app_vectors.dart
@@ -0,0 +1,6 @@
+class AppVectors{
+
+  static const String basePathLogo = 'assets/vectors/logos/';
+  static const String format = '.svg';
+  static const String logo = '${basePathLogo}logo$format';
+}
