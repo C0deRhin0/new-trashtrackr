973f7fea0c0b2ac2ef2f26b868582a7b34e645ae
C0deRhin0 <pauloperez9754@gmail.com>
2024-09-28 09:22:20 +0800

    Authentication logic and Service Locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:new_trashtrackr/presentation/pages/splash/splash.dart';
 import 'package:new_trashtrackr/presentation/pages/auth/signin.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'service_locator.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await initializeDependencies();
   runApp(const MyApp());
 }
 
